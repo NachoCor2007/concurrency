0a91cff7f194c714e37b21996d613f1b36cd349f
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2025-03-18 17:11:07 -0300

    Experimenting with threads

--- examples/rust/hello/src/main.rs
+++ examples/rust/hello/src/main.rs
@@ -8,8 +8,20 @@ fn hello() {
 
 fn main() {
     println!("====== Hello Threads    =======");
-    hello();
+    scoped_threads();
     println!("====== Good Bye Threads =======");
 
     // uncomment me! thread::sleep(Duration::from_secs(1));
+}
+
+fn no_scope_threads() {
+    thread::spawn(|| println!("Hello from no scoped thread 1"));
+    thread::spawn(|| println!("Hello from no scoped thread 2"));
+}
+
+fn scoped_threads() {
+    thread::scope(|s| {
+        s.spawn(|| println!("Hello from scoped thread 1"));
+        s.spawn(|| println!("Hello from scoped thread 2"));
+    })
 }
